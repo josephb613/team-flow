c963c3ec12b3c267aa57f0bf7e4d1e3aba0ae026
Z User <z@container>
2026-06-09 17:56:05 +0000

    535e5a8c-0e74-4579-bc8b-3390c100c5b8

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,11 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
+  allowedDevOrigins: [
+    'http://localhost:81',
+    'http://21.0.14.224:3000',
+    'http://21.0.14.224:81',
+  ],
 };
 
 export default nextConfig;
